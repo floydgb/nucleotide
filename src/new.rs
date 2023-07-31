a7e02806b914fc73dbe9b54026cd93a90ab5cd92
greg floyd <greg.floyd@showerstream.net>
2023-07-30 22:16:43 -0500

    moar cleanup

--- src/new.rs
+++ src/new.rs
@@ -16,86 +16,71 @@ use {
 };
 
 // Types ----------------------------------------------------------------------
-type SequenceCounts = HashMap<Code, u32>;
+type NucleotideCounts = HashMap<Nucleotide, u32>;
 
-struct CodeIter<'a> {
-    iter: Iter<'a, u8>,
-    code: Code,
-    mask: u64,
+struct Genome<'a> {
+    occurance: Nucleotide,
+    cursor: Iter<'a, u8>,
+    cursor_size: usize,
 }
 
 #[derive(Hash, PartialEq, PartialOrd, Ord, Eq, Clone, Copy)]
-struct Code {
+struct Nucleotide {
     code: u64,
 }
 
 // Constants ------------------------------------------------------------------
 const FILE_NAME: &str = "250000_in";
 const FILE_START: &str = ">THREE";
-const OCCS: [&str; 5] = [
+const MAX_BUFFER_SIZE: usize = 65536;
+const MAX_LINE_SIZE: usize = 80;
+const NUCLEOTIDES: [&str; 5] = [
     "GGTATTTTAATTTATAGT",
     "GGTATTTTAATT",
     "GGTATT",
     "GGTA",
     "GGT",
 ];
+const A_CODE: u8 = 0;
+const C_CODE: u8 = 1;
+const T_CODE: u8 = 2;
+const G_CODE: u8 = 3;
 
 // Methods --------------------------------------------------------------------
-impl Code {
-    fn push(&mut self, c: u8, mask: u64) {
+impl Nucleotide {
+    fn push(&mut self, byte: u8, cursor_size: usize) {
         self.code <<= 2;
-        self.code |= c as u64;
-        self.code &= mask;
+        self.code |= byte as u64;
+        self.code &= (1u64 << (2 * cursor_size)) - 1;
     }
 
-    fn from_str(s: &str) -> Code {
-        let mask = make_mask(s.len());
-        let mut res = Code { code: 0 };
-        for c in s.as_bytes() {
-            res.push(encode_byte(c), mask);
-        }
-        res
-    }
-
-    fn to_string(self, frame: usize) -> String {
-        let mut res = vec![];
-        let mut code = self.code;
-        for _ in 0..frame {
-            let c = match code as u8 & 0b11 {
-                c if c == encode_byte(&b'A') => b'A',
-                c if c == encode_byte(&b'T') => b'T',
-                c if c == encode_byte(&b'G') => b'G',
-                c if c == encode_byte(&b'C') => b'C',
+    fn to_string(self, cursor_size: usize) -> String {
+        let mut result = String::default();
+        let mut code = self.code as u8;
+        for _ in 0..cursor_size {
+            let c = match code & 0b11 {
+                A_CODE => b'A',
+                T_CODE => b'T',
+                G_CODE => b'G',
+                C_CODE => b'C',
                 _ => unreachable!(),
             };
-            res.push(c);
+            result.push(c.into());
             code >>= 2;
         }
-        res.reverse();
-        String::from_utf8(res).unwrap()
+        result.chars().rev().collect()
     }
 }
 
-impl<'a> CodeIter<'a> {
-    fn new(input: &[u8], frame: usize) -> CodeIter {
-        let mut iter = input.iter();
-        let mut code = Code { code: 0 };
-        let mask = make_mask(frame);
-        for c in iter.by_ref().take(frame - 1) {
-            code.push(*c, mask);
-        }
-        CodeIter { iter, code, mask }
-    }
-}
 
 // Traits ---------------------------------------------------------------------
-impl<'a> Iterator for CodeIter<'a> {
-    type Item = Code;
+impl<'a> Iterator for Genome<'a> {
+    type Item = Nucleotide;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter.next().map(|&c| {
-            self.code.push(c, self.mask);
-            self.code
+    fn next(&mut self) -> Option<Nucleotide> {
+        self.cursor.next().map(|&byte| {
+            self.occurance.push(byte, self.cursor_size);
+            self.occurance
         })
     }
 }
@@ -106,90 +91,94 @@ fn encode_byte(c: &u8) -> u8 {
     (c >> 1) & 0b11
 }
 
-fn make_mask(frame: usize) -> u64 {
-    (1u64 << (2 * frame)) - 1
+fn new_genome(cursor_size: usize, bytes: &[u8]) -> Genome {
+    let mut iter = bytes.iter();
+    let mut code = Nucleotide { code: 0 };
+    for c in iter.by_ref().take(cursor_size - 1) {
+        code.push(*c, cursor_size);
+    }
+    Genome {
+        cursor: iter,
+        occurance: code,
+        cursor_size,
+    }
+}
+
+fn from_str(nucleotide: &str) -> Nucleotide {
+    let mut result = Nucleotide { code: 0 };
+    for byte in nucleotide.as_bytes() {
+        result.push(encode_byte(byte), nucleotide.len());
+    }
+    result
 }
 
 fn read_file() -> Vec<u8> {
-    if let Ok(file) = File::open(FILE_NAME) {
-        let mut reader = BufReader::new(file);
-        let mut result = Vec::with_capacity(65536);
-        let mut line = String::with_capacity(64);
-
-        loop {
-            match reader.read_line(&mut line) {
-                Ok(b) if b > 0 => {
-                    if line.starts_with(FILE_START) {
-                        break;
-                    }
-                }
-                _ => break,
-            }
-            line.clear();
+    let mut reader = BufReader::new(File::open(FILE_NAME).unwrap());
+    let mut result = Vec::with_capacity(MAX_BUFFER_SIZE);
+    let mut line = String::with_capacity(MAX_LINE_SIZE);
+    while let Ok(_) = reader.read_line(&mut line) {
+        if line.starts_with(FILE_START) {
+            break;
         }
-
-        loop {
-            line.clear();
-            match reader.read_line(&mut line) {
-                Ok(b) if b > 0 => {
-                    let bytes = line.as_bytes();
-                    result.extend(bytes[..bytes.len() - 1].iter().map(encode_byte))
-                }
-                _ => break,
-            }
-        }
-        result
-    } else {
-        unreachable!("File not found");
+        line.clear();
     }
+    line.clear();
+    while let Ok(b) = reader.read_line(&mut line) {
+        if b == 0 {
+            break; 
+        }
+        let bytes = line.as_bytes();
+        result.extend(bytes[..bytes.len() - 1].iter().map(encode_byte));
+        line.clear();
+    }
+    result
 }
 
-fn gen_freq(input: &[u8], frame: usize) -> SequenceCounts {
-    let mut freq = SequenceCounts::default();
-    for code in CodeIter::new(input, frame) {
-        *freq.entry(code).or_insert(0) += 1;
+fn count_occurances(frame_size: usize, genome: &[u8]) -> NucleotideCounts {
+    let mut counts = NucleotideCounts::default();
+    for code in new_genome(frame_size, genome) {
+        *counts.entry(code).or_insert(0) += 1;
     }
-    freq
+    counts
 }
 
-fn print_percents(n: usize, hmap: &SequenceCounts) {
+fn print_percents(frame_size: usize, genome: &[u8]) {
+    let hmap = &count_occurances(frame_size, &genome);
     let total = hmap.values().sum::<u32>() as f32;
     let mut v: Vec<_> = hmap.iter().collect();
     v.sort_by(|a, b| b.1.cmp(a.1));
     for &(code, count) in v.iter() {
         let freq_percent = *count as f32 * 100.0 / total;
-        println!("{} {:.3}", code.to_string(n), freq_percent);
+        println!("{} {:.3}", code.to_string(frame_size), freq_percent);
     }
     println!();
 }
 
-fn print_counts(s: &str, hmap: &SequenceCounts) {
-    let code = Code::from_str(s);
-    if hmap.contains_key(&code) {
-        println!("{}\t{}", hmap[&code], s);
+fn print_counts(occurance: &str, counts: &NucleotideCounts) {
+    let code = from_str(occurance);
+    if counts.contains_key(&code) {
+        println!("{}\t{}", counts[&code], occurance);
     } else {
-        println!("{}\t{}", 0, s);
+        println!("{}\t{}", 0, occurance);
     };
 }
 
 pub fn run() {
     let genome = Arc::new(read_file());
 
-    // In reverse to spawn big tasks first
-    let threads: Vec<_> = OCCS
+    let workers: Vec<_> = NUCLEOTIDES
         .into_iter()
-        .map(|occ| {
-            let input = genome.clone();
-            thread::spawn(move || (occ, gen_freq(&input, occ.len())))
+        .map(|occurance| {
+            let genome = genome.clone();
+            thread::spawn(move || (occurance, count_occurances(occurance.len(), &genome)))
         })
         .collect();
 
-    print_percents(1, &gen_freq(&genome, 1));
-    print_percents(2, &gen_freq(&genome, 2));
-
-    for t in threads.into_iter().rev() {
-        if let Ok((occ, freq)) = t.join() {
-            print_counts(occ, &freq);
+    print_percents(1, &genome);
+    print_percents(2, &genome);
+    for t in workers.into_iter().rev() {
+        if let Ok((occurance, counts)) = t.join() {
+            print_counts(occurance, &counts);
         }
     }
 }
