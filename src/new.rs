0287cc13c5a2858dadbee95f3a1d3e45335a4d29
greg floyd <greg.floyd@showerstream.net>
2023-08-01 14:50:20 -0500

    fewer printlns, faster, fewer LOC

--- src/new.rs
+++ src/new.rs
@@ -23,7 +23,7 @@ struct Nucleotide {
 
 struct GenomeIter<'a> {
     nucleotide: Nucleotide,
-    nucleotide_len: usize,
+    k_len: usize,
     bytes: Iter<'a, u8>,
 }
 
@@ -42,24 +42,27 @@ const NUCLEOTIDE_STRS: [&str; 5] = #[rustfmt::skip] {
 
 // Public Functions -----------------------------------------------------------
 pub fn run() {
-    let genome_arc = read_file();
-    let worker_threads = par_count(&genome_arc);
-    print_percentages(1, &genome_arc);
-    print_percentages(2, &genome_arc);
-    print_counts(worker_threads);
+    let genome = read_file(FILE_NAME);
+    let worker_threads = par_count(&genome);
+    println!(
+        "{}\n\n{}\n\n{}",
+        show_percents(1, &genome),
+        show_percents(2, &genome),
+        show_counts(worker_threads)
+    );
 }
 
 // Private Methods ------------------------------------------------------------
 impl Nucleotide {
-    fn push_byte(&mut self, byte: u8, nucleotide_len: usize) {
+    fn push_byte(&mut self, byte: u8, k_len: usize) {
         self.key <<= 2;
         self.key |= ((byte >> 1) & 0b11) as u64;
-        self.key &= (1u64 << (2 * nucleotide_len)) - 1;
+        self.key &= (1u64 << (2 * k_len)) - 1;
     }
 
-    fn to_str(&self, nucleotide_len: usize) -> String {
-        let mut result = String::with_capacity(nucleotide_len);
-        for i in (0..nucleotide_len).rev() {
+    fn to_str(&self, k_len: usize) -> String {
+        let mut result = String::with_capacity(k_len);
+        for i in (0..k_len).rev() {
             result.push(NUCLEOTIDES[((self.key >> (2 * i)) & 0b11) as usize]);
         }
         result
@@ -80,19 +83,19 @@ impl<'a> Iterator for GenomeIter<'a> {
 
     fn next(&mut self) -> Option<Nucleotide> {
         self.bytes.next().map(|&byte| {
-            self.nucleotide.push_byte(byte, self.nucleotide_len);
+            self.nucleotide.push_byte(byte, self.k_len);
             self.nucleotide
         })
     }
 }
 
 // Private Functions ----------------------------------------------------------
-fn read_file() -> GenomeData {
-    let mut reader = BufReader::new(File::open(FILE_NAME).unwrap());
+fn read_file(file_name: &str) -> GenomeData {
+    let mut buf = BufReader::new(File::open(file_name).expect("file found"));
     let mut bytes = Vec::with_capacity(FILE_BUFFER_SIZE);
     let mut line = Vec::with_capacity(FILE_LINE_SIZE);
     let mut is_genome = false;
-    while let Ok(b) = reader.read_until(b'\n', &mut line) {
+    while let Ok(b) = buf.read_until(b'\n', &mut line) {
         match (is_genome, b, line.starts_with(FILE_START.as_bytes())) {
             (true, 0, _) => break,
             (true, _, _) => bytes.extend_from_slice(&line[..b - 1]),
@@ -104,42 +107,34 @@ fn read_file() -> GenomeData {
     Arc::new(bytes)
 }
 
-fn build_iter(nucleotide_len: usize, genome: &GenomeData) -> GenomeIter {
-    let mut bytes = genome.iter();
+fn build_iter(k_len: usize, genome: &GenomeData) -> GenomeIter {
+    #[rustfmt::skip] { let mut bytes = genome.iter();
     let mut nucleotide = Nucleotide::default();
-    for byte in bytes.by_ref().take(nucleotide_len - 1) {
-        nucleotide.push_byte(*byte, nucleotide_len);
-    }
-    #[rustfmt::skip] {GenomeIter {nucleotide, nucleotide_len, bytes}}
+    for byte in bytes.by_ref().take(k_len - 1) {
+        nucleotide.push_byte(*byte, k_len);
+    } 
+    GenomeIter {nucleotide, k_len, bytes}}
 }
 
-fn count(nucleotide_len: usize, genome: &GenomeData) -> NucleotideCounts {
+fn count(k_len: usize, genome: &GenomeData) -> NucleotideCounts {
     let mut table = NucleotideCounts::default();
-    for nucleotide in build_iter(nucleotide_len, genome) {
+    for nucleotide in build_iter(k_len, genome) {
         *table.entry(nucleotide).or_insert(0) += 1;
     }
     table
 }
 
-fn print_percentages(nucleotide_len: usize, genome: &GenomeData) {
-    let table = count(nucleotide_len, &genome);
-    for (nucleotide, percentage) in calc_percentages(&table) {
-        println!("{} {:.3}", nucleotide.to_str(nucleotide_len), percentage);
-    }
-    println!();
+fn par_count(genome: &GenomeData) -> ThreadPool {
+    Iterator::collect(NUCLEOTIDE_STRS.into_iter().map(|nucleotide| {
+        let genome = Arc::clone(genome);
+        thread::spawn(move || (nucleotide, count(nucleotide.len(), &genome)))
+    }))
 }
 
-fn par_count(genome: &GenomeData) -> ThreadPool {
-    NUCLEOTIDE_STRS
-        .into_iter()
-        .map(|nucleotide| {
-            let genome = Arc::clone(genome);
-            thread::spawn(move || {
-                #[rustfmt::skip]
-                {(nucleotide, count(nucleotide.len(), &genome))}
-            })
-        })
-        .collect()
+fn sort_by_count(table: &NucleotideCounts) -> Vec<(&Nucleotide, &u32)> {
+    let mut sorted_table: Vec<_> = table.iter().collect();
+    sorted_table.sort_by(|(_, lhs), (_, rhs)| rhs.cmp(lhs));
+    sorted_table
 }
 
 fn calc_percentages(table: &NucleotideCounts) -> Vec<(&Nucleotide, f32)> {
@@ -152,17 +147,18 @@ fn calc_percentages(table: &NucleotideCounts) -> Vec<(&Nucleotide, f32)> {
     percent_table
 }
 
-fn sort_by_count(table: &NucleotideCounts) -> Vec<(&Nucleotide, &u32)> {
-    let mut sorted_table: Vec<(&Nucleotide, &u32)> = table.iter().collect();
-    sorted_table.sort_by(|(_, lhs), (_, rhs)| rhs.cmp(lhs));
-    sorted_table
+fn show_percents(k_len: usize, genome: &GenomeData) -> String {
+    #[rustfmt::skip] { Iterator::collect::<Vec<_>>(calc_percentages(&count(k_len, &genome)).iter()
+        .map(|(nucleotide, percentage)| {
+            format!("{} {:.3}", nucleotide.to_str(k_len), percentage)}))
+        .join("\n")}
 }
 
-fn print_counts(worker_threads: ThreadPool) {
-    for thread in worker_threads.into_iter().rev() {
-        if let Ok((nucleotide, counts)) = thread.join() {
-            let count = counts[&Nucleotide::from(nucleotide)];
-            println!("{}\t{}", count, nucleotide);
-        }
-    }
+fn show_counts(threads: ThreadPool) -> String {
+    #[rustfmt::skip] { Iterator::collect::<Vec<String>>(threads.into_iter().rev()
+        .map(|thread| { match thread.join().expect("threads halt") {
+            (nucleotide, counts) => {
+                let count = counts[&Nucleotide::from(nucleotide)];
+                format!("{}\t{}", count, nucleotide).into()}}}))
+        .join("\n")}
 }
