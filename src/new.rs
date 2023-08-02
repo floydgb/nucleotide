1c0f57d041d4ff8cfefe9c12720ec89a9a4ac8f3
greg floyd <greg.floyd@showerstream.net>
2023-08-01 20:55:58 -0500

    renames, better fn parameter types

--- src/new.rs
+++ src/new.rs
@@ -14,27 +14,27 @@ use {
         io::{BufRead, BufReader},
         slice::Iter,
         sync::Arc,
-        thread,
+        thread::{spawn, JoinHandle},
     },
 };
 
 // Types ----------------------------------------------------------------------
 #[derive(Hash, Default, PartialEq, PartialOrd, Ord, Eq, Clone, Copy)]
-struct SeqHash {
-    key: u64,
+struct Seq {
+    hash_key: u64,
 }
 
 struct GenomeIter<'a> {
-    seq: SeqHash,
-    k_len: usize,
-    bytes: Iter<'a, u8>,
+    seq_len: usize,
+    needle: Seq,
+    haystack: Iter<'a, u8>,
 }
 
-type GenomeData = Arc<Vec<u8>>;
-type SeqCounts = HashMap<SeqHash, u32>;
-type SortedSeqCounts = Vec<(SeqHash, u32)>;
-type SeqPercents = Vec<(SeqHash, f32)>;
-type ThreadPool = Vec<thread::JoinHandle<(&'static str, SeqCounts)>>;
+type Genome = Arc<Vec<u8>>;
+type SeqCnts = HashMap<Seq, u32>;
+type SeqCntsSort = Vec<(Seq, u32)>;
+type SeqPcts = Vec<(Seq, f32)>;
+type ThrdPool = Vec<JoinHandle<(&'static str, SeqCnts)>>;
 
 // Constants ------------------------------------------------------------------
 const FILE_NAME: &str = "250000_in";
@@ -42,45 +42,40 @@ const FILE_START: &str = ">THREE";
 const FILE_BUFFER_SIZE: usize = 65536;
 const FILE_LINE_SIZE: usize = 64;
 const DECIMALS: usize = 3;
-const NUCLEOTIDES: [char; 4] = ['A', 'C', 'T', 'G'];
-const SEQS: [&str; 5] = [
-    "GGTATTTTAATTTATAGT",
-    "GGTATTTTAATT",
-    "GGTATT",
-    "GGTA",
-    "GGT",
-];
+const SEQ_STRS: [&str; 5] = #[rustfmt::skip] {
+        ["GGTATTTTAATTTATAGT", "GGTATTTTAATT", "GGTATT", "GGTA", "GGT"]};
 
 // Public Functions -----------------------------------------------------------
 pub fn run() {
     let genome = read_file(FILE_NAME);
-    let worker_threads = par_seq_count(&genome);
-    println!(
-        "{}\n\n{}\n\n{}",
-        show_seq_percents(1, &genome),
-        show_seq_percents(2, &genome),
-        show_seq_counts(worker_threads)
+    let worker_thrds = par_seq_cnt(&genome);
+    print!(
+        "{}\n\n{}\n\n{}\n",
+        show_seq_pcts(genome_iter(1, &genome)),
+        show_seq_pcts(genome_iter(2, &genome)),
+        show_seq_cnts(worker_thrds)
     );
 }
 
 // Private Methods ------------------------------------------------------------
-impl SeqHash {
-    fn push_byte(&mut self, byte: u8, k_len: usize) {
-        self.key <<= 2;
-        self.key |= ((byte >> 1) & 0b11) as u64;
-        self.key &= (1u64 << (2 * k_len)) - 1;
+impl Seq {
+    fn push_byte(&mut self, byte: u8, seq_len: usize) {
+        self.hash_key <<= 2;
+        self.hash_key |= ((byte >> 1) & 0b11) as u64;
+        self.hash_key &= (1u64 << (2 * seq_len)) - 1;
     }
 
-    fn to_str(&self, k_len: usize) -> String {
-        let mut result = String::with_capacity(k_len);
-        for i in (0..k_len).rev() {
-            result.push(NUCLEOTIDES[((self.key >> (2 * i)) & 0b11) as usize]);
+    fn to_str(self, seq_len: usize) -> String {
+        let nucleotide = ['A', 'C', 'T', 'G'];
+        let mut str = String::with_capacity(seq_len);
+        for i in (0..seq_len).rev() {
+            str.push(nucleotide[((self.hash_key >> (2 * i)) & 0b11) as usize]);
         }
-        result
+        str
     }
 
-    fn from(seq_str: &str) -> SeqHash {
-        let mut seq = SeqHash::default();
+    fn from(seq_str: &str) -> Seq {
+        let mut seq = Seq::default();
         for byte in seq_str.as_bytes() {
             seq.push_byte(*byte, seq_str.len());
         }
@@ -90,27 +85,27 @@ impl SeqHash {
 
 // Private Traits -------------------------------------------------------------
 impl<'a> Iterator for GenomeIter<'a> {
-    type Item = SeqHash;
+    type Item = Seq;
 
-    fn next(&mut self) -> Option<SeqHash> {
-        self.bytes.next().map(|&byte| {
-            self.seq.push_byte(byte, self.k_len);
-            self.seq
+    fn next(&mut self) -> Option<Seq> {
+        self.haystack.next().map(|&byte| {
+            self.needle.push_byte(byte, self.seq_len);
+            self.needle
         })
     }
 }
 
 // Private Functions ----------------------------------------------------------
-fn read_file(file_name: &str) -> GenomeData {
+fn read_file(file_name: &str) -> Genome {
     let mut buf = BufReader::new(File::open(file_name).expect("file found"));
     let mut bytes = Vec::with_capacity(FILE_BUFFER_SIZE);
     let mut line = Vec::with_capacity(FILE_LINE_SIZE);
-    let mut is_genome = false;
+    let mut genome_start = false;
     while let Ok(b) = buf.read_until(b'\n', &mut line) {
-        match (is_genome, b, line.starts_with(FILE_START.as_bytes())) {
+        match (genome_start, b, line.starts_with(FILE_START.as_bytes())) {
             (true, 0, _) => break,
             (true, _, _) => bytes.extend_from_slice(&line[..b - 1]),
-            (false, _, true) => is_genome = true,
+            (false, _, true) => genome_start = true,
             _ => (),
         }
         line.clear();
@@ -118,60 +113,59 @@ fn read_file(file_name: &str) -> GenomeData {
     Arc::new(bytes)
 }
 
-fn genome_iter(k_len: usize, genome: &GenomeData) -> GenomeIter {
-    let mut bytes = genome.iter();
-    let mut seq = SeqHash::default();
-    for byte in bytes.by_ref().take(k_len - 1) {
-        seq.push_byte(*byte, k_len);
+fn genome_iter(seq_len: usize, genome: &Genome) -> GenomeIter {
+    let mut haystack = genome.iter();
+    let mut needle = Seq::default();
+    for byte in haystack.by_ref().take(seq_len - 1) {
+        needle.push_byte(*byte, seq_len);
     }
-    GenomeIter { seq, k_len, bytes }
+    #[rustfmt::skip] {GenomeIter {seq_len, needle, haystack}}
 }
 
-fn seq_count(k_len: usize, genome: &GenomeData) -> SeqCounts {
-    let mut seq_counts = SeqCounts::default();
-    for seq in genome_iter(k_len, genome) {
-        *seq_counts.entry(seq).or_insert(0) += 1;
+fn seq_cnt(g_iter: GenomeIter) -> SeqCnts {
+    let mut seq_cnts = SeqCnts::default();
+    for seq in g_iter {
+        *seq_cnts.entry(seq).or_insert(0) += 1;
     }
-    seq_counts
+    seq_cnts
 }
 
-fn par_seq_count(genome: &GenomeData) -> ThreadPool {
-    Iterator::collect(SEQS.into_iter().map(|seq_str| {
+fn par_seq_cnt(genome: &Genome) -> ThrdPool {
+    Iterator::collect(SEQ_STRS.into_iter().map(|seq_str| {
         let genome = Arc::clone(genome);
-        thread::spawn(move || (seq_str, seq_count(seq_str.len(), &genome)))
+        spawn(move || (seq_str, seq_cnt(genome_iter(seq_str.len(), &genome))))
     }))
 }
 
-fn sort_by_count(seq_counts: SeqCounts) -> SortedSeqCounts {
-    let mut sorted_seqs: SortedSeqCounts = seq_counts.into_iter().collect();
-    sorted_seqs.sort_by(|(_, lhs), (_, rhs)| rhs.cmp(lhs));
-    sorted_seqs
+fn sort_by_cnt(seq_cnts: SeqCnts) -> SeqCntsSort {
+    let mut seq_cnts_sort: SeqCntsSort = seq_cnts.into_iter().collect();
+    seq_cnts_sort.sort_by(|(_, l_cnt), (_, r_cnt)| r_cnt.cmp(l_cnt));
+    seq_cnts_sort
 }
 
-fn calc_percentages(seq_counts: SeqCounts) -> SeqPercents {
-    let mut seq_percents = Vec::default();
-    let total_seqs: u32 = seq_counts.values().sum();
-    for (seq, count) in sort_by_count(seq_counts) {
-        let percent = count as f32 / total_seqs as f32 * 100_f32;
-        seq_percents.push((seq, percent));
+fn calc_pcts(seq_cnts: SeqCnts) -> SeqPcts {
+    let tot_seqs: u32 = seq_cnts.values().sum();
+    let mut seq_pcts = Vec::default();
+    for (seq, cnt) in sort_by_cnt(seq_cnts) {
+        seq_pcts.push((seq, cnt as f32 / tot_seqs as f32 * 100_f32));
     }
-    seq_percents
+    seq_pcts
 }
 
-fn show_seq_percents(k_len: usize, genome: &GenomeData) -> String {
-    let percents = calc_percentages(seq_count(k_len, &genome));
-    let mut output = Vec::default();
-    for (seq, percent) in percents {
-        output.push(format!("{} {:.*}", seq.to_str(k_len), DECIMALS, percent));
+fn show_seq_pcts(g_iter: GenomeIter) -> String {
+    let (seq_len, seq_cnts) = (g_iter.seq_len, seq_cnt(g_iter));
+    let mut str = Vec::default();
+    for (seq, pct) in calc_pcts(seq_cnts) {
+        str.push(format!("{} {:.*}", seq.to_str(seq_len), DECIMALS, pct));
     }
-    output.join("\n")
+    str.join("\n")
 }
 
-fn show_seq_counts(threads: ThreadPool) -> String {
-    let mut output = Vec::default();
-    for thread in threads.into_iter().rev() {
-        let (seq_str, counts) = thread.join().expect("threads halt");
-        output.push(format!("{}\t{}", counts[&SeqHash::from(seq_str)], seq_str));
+fn show_seq_cnts(thrds: ThrdPool) -> String {
+    let mut str = Vec::default();
+    for thrd in thrds.into_iter().rev() {
+        let (seq_str, seq_cnts) = thrd.join().expect("thread halts");
+        str.push(format!("{}\t{}", seq_cnts[&Seq::from(seq_str)], seq_str));
     }
-    output.join("\n")
+    str.join("\n")
 }
