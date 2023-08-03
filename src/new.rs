9af8f6d99e4aa172f043a17a1c7c1b1fee15cf55
greg floyd <greg.floyd@showerstream.net>
2023-08-03 05:02:22 -0500

    cleanup, add sort fn

--- src/new.rs
+++ src/new.rs
@@ -4,7 +4,6 @@
 // contributed by Greg Floyd
 
 // Based on k-nucleotide Rust #8
-// Basic sanity & legibility
 
 // Imports --------------------------------------------------------------------
 #[rustfmt::skip] 
@@ -31,23 +30,27 @@ type ThreadPool = Vec<thread::JoinHandle<(String, HashMap<Sequence, u32>)>>;
 // Public Functions -----------------------------------------------------------
 #[rustfmt::skip]
 pub fn run() {
-    let genome = read_genome_file("250000_in");
+    let genome = read_file("250000_in");
     let seqs = str!["GGT","GGTA","GGTATT","GGTATTTTAATT","GGTATTTTAATTTATAGT"];
-    let threads = count_sequences_par(seqs, &genome);
-    println!("{}\n", show_sequences(1, count_sequences(1, &genome)));
-    println!("{}\n", show_sequences(2, count_sequences(2, &genome)));
-    println!("{}", show_sequences_par(threads));
+
+    let seqs_cnt = count(seqs, &genome);
+    let k1_seqs_pct = count_k(1, &genome);
+    let k2_seqs_pct = count_k(2, &genome);
+
+    println!("{}\n", show_k(1, k1_seqs_pct));
+    println!("{}\n", show_k(2, k2_seqs_pct));
+    println!("{}", show(seqs_cnt));
 }
 
 // Private Functions ----------------------------------------------------------
 impl Sequence {
-    fn push_byte(&mut self, byte: u8, seq_len: usize) {
+    fn push(&mut self, byte: u8, seq_len: usize) {
         self.hash_key <<= 2;
         self.hash_key |= ((byte >> 1) & 0b11) as u64;
         self.hash_key &= (1u64 << (2 * seq_len)) - 1;
     }
 
-    fn into(self, seq_len: usize) -> String {
+    fn to_str(self, seq_len: usize) -> String {
         const NUCLEOTIDE: [char; 4] = ['A', 'C', 'T', 'G'];
         let mut str = String::new();
         for i in (0..seq_len).rev() {
@@ -55,14 +58,14 @@ impl Sequence {
         }
         str
     }
+}
 
-    fn from(seq_str: &str) -> Sequence {
-        let mut seq = Sequence::default();
-        for byte in seq_str.as_bytes() {
-            seq.push_byte(*byte, seq_str.len());
-        }
-        seq
+fn from_str(seq_str: &str) -> Sequence {
+    let mut seq = Sequence::default();
+    for byte in seq_str.as_bytes() {
+        seq.push(*byte, seq_str.len());
     }
+    seq
 }
 
 impl<'a> Iterator for GenomeIter<'a> {
@@ -70,13 +73,13 @@ impl<'a> Iterator for GenomeIter<'a> {
 
     fn next(&mut self) -> Option<Sequence> {
         self.haystack.next().map(|&byte| {
-            self.needle.push_byte(byte, self.seq_len);
+            self.needle.push(byte, self.seq_len);
             self.needle
         })
     }
 }
 
-fn read_genome_file(file_name: &str) -> Genome {
+fn read_file(file_name: &str) -> Genome {
     let mut buf = BufReader::new(File::open(file_name).expect("file found"));
     let (mut bytes, mut line, mut start) = (Vec::new(), Vec::new(), false);
     while buf.read_until(b'\n', &mut line).expect("read line") > 0 {
@@ -92,12 +95,12 @@ fn read_genome_file(file_name: &str) -> Genome {
 fn genome_iter(seq_len: usize, genome: &Genome) -> GenomeIter {
     let (mut haystack, mut needle) = (genome.iter(), Sequence::default());
     for byte in haystack.by_ref().take(seq_len - 1) {
-        needle.push_byte(*byte, seq_len);
+        needle.push(*byte, seq_len);
     }
     #[rustfmt::skip] GenomeIter {seq_len, needle, haystack}
 }
 
-fn count_sequences(seq_len: usize, genome: &Genome) -> HashMap<Sequence, u32> {
+fn count_k(seq_len: usize, genome: &Genome) -> HashMap<Sequence, u32> {
     let mut seq_cnts = HashMap::<Sequence, u32>::new();
     for seq in genome_iter(seq_len, genome) {
         *seq_cnts.entry(seq).or_insert(0) += 1;
@@ -105,37 +108,40 @@ fn count_sequences(seq_len: usize, genome: &Genome) -> HashMap<Sequence, u32> {
     seq_cnts
 }
 
-fn count_sequences_par(seq_strs: Vec<String>, genome: &Genome) -> ThreadPool {
+fn count(seq_strs: Vec<String>, genome: &Genome) -> ThreadPool {
     Iterator::collect(seq_strs.into_iter().map(|str| {
         let (seq_len, genome) = (str.len(), Arc::clone(genome));
-        thread::spawn(move || (str, count_sequences(seq_len, &genome)))
+        thread::spawn(move || (str, count_k(seq_len, &genome)))
     }))
 }
 
 fn calc_percents(seq_cnts: HashMap<Sequence, u32>) -> Vec<(Sequence, f32)> {
     let (mut pcts, tot_seqs) = (Vec::new(), seq_cnts.values().sum::<u32>());
-    let mut seq_cnts_sort: Vec<_> = seq_cnts.into_iter().collect();
-    seq_cnts_sort.sort_by(|(_, l_cnt), (_, r_cnt)| r_cnt.cmp(l_cnt));
-    for (seq, cnt) in seq_cnts_sort {
+    for (seq, cnt) in sort_by_count(seq_cnts) {
         pcts.push((seq, cnt as f32 / tot_seqs as f32 * 100_f32));
     }
     pcts
 }
 
-fn show_sequences(seq_len: usize, seq_cnts: HashMap<Sequence, u32>) -> String {
+fn sort_by_count(seq_cnts: HashMap<Sequence, u32>) -> Vec<(Sequence, u32)> {
+    let mut seq_cnts_sort: Vec<_> = seq_cnts.into_iter().collect();
+    seq_cnts_sort.sort_by(|(_, l_cnt), (_, r_cnt)| r_cnt.cmp(l_cnt));
+    seq_cnts_sort
+}
+
+fn show_k(seq_len: usize, seq_cnts: HashMap<Sequence, u32>) -> String {
     let mut str = Vec::new();
     for (seq, pct) in calc_percents(seq_cnts) {
-        str.push(format!("{} {:.3}", seq.into(seq_len), pct));
+        str.push(format!("{} {:.3}", seq.to_str(seq_len), pct));
     }
     str.join("\n")
 }
 
-fn show_sequences_par(pool: ThreadPool) -> String {
+fn show(pool: ThreadPool) -> String {
     let mut str = Vec::new();
     for thrd in pool.into_iter().rev() {
         let (seq_str, seq_cnts) = thrd.join().expect("thread halts");
-        let count = seq_cnts.get(&Sequence::from(&seq_str)).unwrap_or(&0);
-        str.push(format!("{}\t{}", count, seq_str));
+        str.push(format!("{}\t{}", seq_cnts[&from_str(&seq_str)], seq_str));
     }
     str.join("\n")
 }
