f6f6dd1c079c73c82d0e23d7bfa48224981967cb
greg floyd <greg.floyd@showerstream.net>
2023-08-19 05:41:15 -0500

    more legible

--- src/knucleotide.rs
+++ src/knucleotide.rs
@@ -1,120 +1,170 @@
 // The Computer Language Benchmarks Game
 // https://programming-language-benchmarks.vercel.app/problem/knucleotide
+//
 // contributed by Greg Floyd
+
+// Imports --------------------------------------------------------------------
 use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 use std::io::{BufRead, BufReader};
 use std::{fs::File, slice::Iter, sync::Arc, thread::spawn, thread::JoinHandle};
 use {crate::str, hashbrown::HashMap, std::vec::IntoIter};
+
+// Types ----------------------------------------------------------------------
 #[derive(Hash, Default, PartialEq, Eq, Clone, Copy)]
-pub struct Seq {
+pub struct Sequence {
     key: u64,
 }
-pub struct KNucleotides<'a> {
+pub struct KGenomeIter<'a> {
     k: usize,
-    seq: Seq,
-    gen: Iter<'a, u8>,
+    seq: Sequence,
+    genome: Iter<'a, u8>,
 }
+pub type SeqCounts = HashMap<Sequence, u32>;
 pub type Threads = Vec<JoinHandle<(String, usize)>>;
-#[rustfmt::skip] pub fn main() {
+
+// Main -----------------------------------------------------------------------
+#[rustfmt::skip]
+pub fn main() {
     let genome = read_file("2500000_in"); 
     let seqs = str!["GGT","GGTATTTTAATT","GGTA","GGTATTTTAATTTATAGT","GGTATT"];
-    let counts = count(seqs, &genome);
+
+    let seq_counts = count(seqs, &genome);
     let (k1, k2) = (count_k(1, &genome), count_k(2, &genome));
-    println!("{}\n\n{}\n\n{}", show_k(1, k1), show_k(2, k2), show(counts));
+
+    println!("{}\n\n{}\n\n{}", show_k(1, k1), show_k(2, k2), show(seq_counts));
 }
+
+// Public Functions -----------------------------------------------------------
 pub fn read_file(path: &str) -> Arc<Vec<u8>> {
     let (mut read, mut r) = (false, BufReader::new(File::open(path).unwrap()));
-    let (mut buf, mut l) = (Vec::with_capacity(15000000), Vec::new());
-    #[rustfmt::skip] while r.read_until(b'\n', &mut l).unwrap_or(0) > 0 {
-        if read { buf.extend_from_slice(&l[..l.len() - 1])} 
-        else { read = l.starts_with(">TH".as_bytes())}
-        l.clear();
+    let (mut buf, mut line) = (Vec::with_capacity(15000000), Vec::new());
+    while r.read_until(b'\n', &mut line).unwrap_or(0) > 0 {
+        match read {
+            true => buf.extend_from_slice(&line[..line.len() - 1]),
+            false => read = line.starts_with(">TH".as_bytes()),
+        }
+        line.clear();
     }
     Arc::new(buf)
 }
+
 pub fn count(seqs: Vec<String>, genome: &Arc<Vec<u8>>) -> Threads {
     let mut threads = Vec::with_capacity(seqs.len());
-    sort_len(seqs).for_each(|seq_str| {
+    for str in sort_len(seqs) {
         let arc = Arc::clone(&genome);
-        threads.push(spawn(move || par_count(&seq_str, seq_str.len(), &arc)));
-    });
+        threads.push(spawn(move || par_count(&str, &arc)));
+    }
     threads
 }
-pub fn count_k(k: usize, genome: &[u8]) -> HashMap<Seq, u32> {
-    #[rustfmt::skip] chunks(genome.len() / 64, k - 1, genome)
-        .map(|chunk| inner_count_k(k, chunk)).reduce(HashMap::default, merge)
+
+pub fn count_k(k: usize, genome: &[u8]) -> SeqCounts {
+    chunks(genome.len() / 64, k - 1, genome)
+        .into_par_iter()
+        .map(|chunk| inner_count_k(k, chunk))
+        .reduce(HashMap::default, merge)
 }
+
 pub fn show(counts: Threads) -> String {
     let mut str = Vec::with_capacity(counts.len());
-    counts.into_iter().for_each(|thrd| {
+    for thrd in counts {
         let (seq_str, seq_cnt) = thrd.join().expect("thread halts");
         str.push(format!("{}\t{}", seq_cnt, seq_str));
-    });
-    str.join("\n")
-}
-pub fn show_k(k: usize, c: HashMap<Seq, u32>) -> String {
-    let mut str = Vec::with_capacity(c.len());
-    calc_percents(c.values().sum(), c)
-        .for_each(|(s, p)| str.push(format!("{} {:.3}", s.to_str(k), p)));
-    str.join("\n")
-}
-impl<'a> Iterator for KNucleotides<'a> {
-    type Item = Seq; #[rustfmt::skip]
-    fn next(&mut self) -> Option<Seq> {
-        self.gen.next().map(|&byte| {self.seq.push(byte, self.k); self.seq})
     }
+    str.join("\n")
 }
-impl Seq {
+
+pub fn show_k(k: usize, counts: SeqCounts) -> String {
+    let mut str = Vec::with_capacity(counts.len());
+    for (s, p) in calc_percents(counts.values().sum(), counts) {
+        str.push(format!("{} {:.3}", s.to_str(k), p))
+    }
+    str.join("\n")
+}
+
+// Private Functions ----------------------------------------------------------
+impl Sequence {
     fn push(&mut self, byte: u8, k: usize) {
         self.key = (self.key << 2) | ((byte >> 1) & 0b11) as u64;
         self.key &= (1u64 << (2 * k)) - 1;
     }
+
     fn to_str(self, k: usize) -> String {
         let mut s = String::with_capacity(k);
-        #[rustfmt::skip] (0..k).rev().for_each(|i|
-        s.push(['A', 'C', 'T', 'G'][((self.key >> (2 * i)) & 0b11) as usize]));
+        for i in (0..k).rev() {
+            s.push(['A', 'C', 'T', 'G'][(self.key >> (2 * i) & 0b11) as usize])
+        }
         s
     }
+
     fn from_str(s: &str) -> Self {
         let mut seq = Self::default();
-        s.as_bytes().into_iter().for_each(|b| seq.push(*b, s.len()));
+        for b in s.as_bytes().into_iter() {
+            seq.push(*b, s.len())
+        }
         seq
     }
 }
-fn k_nucl(k: usize, g: &[u8]) -> KNucleotides {
-    #[rustfmt::skip]KNucleotides {k, seq: Seq::default(), gen: g.iter()}
+
+impl<'a> Iterator for KGenomeIter<'a> {
+    type Item = Sequence;
+
+    fn next(&mut self) -> Option<Sequence> {
+        self.seq.push(*self.genome.next()?, self.k);
+        Some(self.seq)
+    }
 }
-fn chunks(s: usize, o: usize, bs: &[u8]) -> rayon::vec::IntoIter<&[u8]> {
-    #[rustfmt::skip]bs.windows(s + o).step_by(s)
-        .collect::<Vec<&[u8]>>().into_par_iter()
+
+#[rustfmt::skip]
+fn k_genome_iter(k: usize, genome: &[u8]) -> KGenomeIter {
+   KGenomeIter {k, seq: Sequence::default(), genome: genome.into_iter()}
 }
-fn par_count(seq: &str, k: usize, genome: &[u8]) -> (String, usize) {
-    #[rustfmt::skip](seq.into(), chunks(genome.len() / 64, k-1, genome)
-        .map(|chunk| inner_count(Seq::from_str(seq), k, chunk)).sum())
+
+fn chunks(len: usize, overlap: usize, genome: &[u8]) -> Vec<&[u8]> {
+    genome.windows(len + overlap).step_by(len).collect()
 }
-fn inner_count(seq: Seq, k: usize, gen: &[u8]) -> usize {
-    k_nucl(k, gen).filter(|&s| s == seq).count()
+
+fn par_count(seq: &str, genome: &[u8]) -> (String, usize) {
+    let count = chunks(genome.len() / 64, seq.len() - 1, genome)
+        .into_par_iter()
+        .map(|chunk| inner_count(Sequence::from_str(seq), seq.len(), chunk))
+        .sum();
+    (seq.into(), count)
 }
-fn inner_count_k(k: usize, genome: &[u8]) -> HashMap<Seq, u32> {
-    let mut seq_cnts = HashMap::with_capacity(4usize.pow(k as u32));
-    k_nucl(k, genome).for_each(|seq| *seq_cnts.entry(seq).or_insert(0) += 1);
-    seq_cnts
+
+fn inner_count(seq: Sequence, k: usize, genome: &[u8]) -> usize {
+    k_genome_iter(k, genome).filter(|&s| s == seq).count()
 }
-fn calc_percents(tot: u32, cnt: HashMap<Seq, u32>) -> IntoIter<(Seq, f32)> {
-    let mut p = Vec::with_capacity(cnt.len());
-    sort_cnt(cnt).for_each(|(s, c)| p.push((s, c as f32 * 100. / tot as f32)));
-    p.into_iter()
+
+fn inner_count_k(k: usize, genome: &[u8]) -> SeqCounts {
+    let mut counts = HashMap::with_capacity(4usize.pow(k as u32));
+    for seq in k_genome_iter(k, genome) {
+        *counts.entry(seq).or_insert(0) += 1
+    }
+    counts
 }
+
+fn calc_percents(total: u32, counts: SeqCounts) -> IntoIter<(Sequence, f32)> {
+    let mut percents = Vec::with_capacity(counts.len());
+    for (seq, count) in sort_cnt(counts) {
+        percents.push((seq, count as f32 * 100. / total as f32))
+    }
+    percents.into_iter()
+}
+
 fn sort_len(mut seqs: Vec<String>) -> IntoIter<String> {
     seqs.sort_by(|l, r| l.len().cmp(&r.len()));
     seqs.into_iter()
 }
-fn sort_cnt(counts: HashMap<Seq, u32>) -> IntoIter<(Seq, u32)> {
-    let mut counts_sorted: Vec<(Seq, u32)> = counts.into_iter().collect();
-    counts_sorted.sort_by(|(_, l_cnt), (_, r_cnt)| r_cnt.cmp(l_cnt));
-    counts_sorted.into_iter()
+
+fn sort_cnt(counts: SeqCounts) -> IntoIter<(Sequence, u32)> {
+    let mut counts: Vec<(Sequence, u32)> = counts.into_iter().collect();
+    counts.sort_by(|(_, l_cnt), (_, r_cnt)| r_cnt.cmp(l_cnt));
+    counts.into_iter()
 }
-fn merge(mut a: HashMap<Seq, u32>, b: HashMap<Seq, u32>) -> HashMap<Seq, u32> {
-    b.iter().for_each(|(s, c)| *a.entry(*s).or_insert(0) += c);
-    a
+
+fn merge(mut l_counts: SeqCounts, r_counts: SeqCounts) -> SeqCounts {
+    for (seq, count) in r_counts.iter() {
+        *l_counts.entry(*seq).or_insert(0) += count
+    }
+    l_counts
 }
